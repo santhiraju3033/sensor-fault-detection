fab9f45a3e84bba2afa55395a828861b86d1cfe7
santhiraju3033 <santhirajudatascience>
2022-11-13 00:07:58 +0530

    Data ingestion stage completed

---
+++ main.py
@@ -0,0 +1,16 @@
+from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os,sys
+from sensor.logger import logging
+from sensor.pipeline.training_pipeline import TrainPipeline
+
+
+
+
+
+
+
+if __name__=='__main__':
+    train_pipeline=TrainPipeline()
+    train_pipeline.run_pipeline()
+    
---
+++ sensor/configuration/mongo_db_connection.py
@@ -0,0 +1,23 @@
+import pymongo
+from sensor.constant.database import DATABASE_NAME
+from sensor.constant.env_variable import MONGODB_URL_KEY
+import os
+
+import certifi
+
+ca = certifi.where()
+
+class MongoDBClient:
+    client = None
+    def __init__(self, database_name=DATABASE_NAME) -> None:
+        try:
+
+            if MongoDBClient.client is None:
+                mongo_db_url=os.getenv(MONGODB_URL_KEY)
+                mongo_db_url = "mongodb+srv://santhi3033:santhi3033@cluster0.f2wchit.mongodb.net/test"
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+        except Exception as e:
+            raise e
